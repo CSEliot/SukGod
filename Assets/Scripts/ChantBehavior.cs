7a659832d429e1c4a47691aa2b3d8c6bbf3c86a3
Chris Ridgely <chridgely@gmail.com>
2016-01-30 10:26:17 -0500

    Starting progress on chanting script

---
+++ Assets/Scripts/ChantBehavior.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class chanting : MonoBehaviour {
+
+	public GameObject characterRB; 
+	public string chantKey;
+	public AudioSource chant;
+	public float chantDuration = 10f;
+	private bool isChanting;
+
+
+
+	// Use this for initialization
+	void Start () {
+		isChanting = false;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+
+	public void startChanting(){
+		if (Input.GetButton (chantKey))
+			isChanting = true;
+	}
+
+	public void startTimer(){
+		
+		chant.Play ();
+
+		for (float time = chantDuration; time > 0; time--);
+
+		chant.Stop ();
+	
+	}
