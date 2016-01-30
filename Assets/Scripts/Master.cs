05177f7f1a9f9e06b0ed1853ed911a37e392442b
Eliot Carney-Seim <ecsit39@gmail.com>
2016-01-30 17:30:37 -0500

    Networking test phase 2

---
+++ Assets/Scripts/Master.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Master : MonoBehaviour {
+
+
+
+    void Awake ()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
