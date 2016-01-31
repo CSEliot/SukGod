53025ecb4b08155259e39d307bcc7dffb1cba36c
Chris Ridgely <chridgely@gmail.com>
2016-01-31 07:35:12 -0500

    Clone Behavior complete

--- Assets/Scripts/CloneBehavior.cs
+++ Assets/Scripts/CloneBehavior.cs
@@ -6,6 +6,7 @@ using System.Collections;
 public class CloneBehavior : MonoBehaviour {
 
 	public Vector3[] locationArray;
+	public Quaternion[] rotationArray;
 	private GameObject player;
 	private float chantDuration;
 	public GameObject clonePlayer;
@@ -18,7 +19,8 @@ public class CloneBehavior : MonoBehaviour {
 		chantDuration = gameObject.GetComponent<ChantBehavior> ().chantDuration;
 		dynamicArraySize = (int)(chantDuration / recordDelay);
 		player = gameObject;
-		locationArray = new Vector3[dynamicArraySize+1];
+		locationArray = new Vector3[dynamicArraySize + 1];
+		rotationArray = new Quaternion[dynamicArraySize + 1];
 	}
 	
 	// Update is called once per frame
@@ -30,11 +32,36 @@ public class CloneBehavior : MonoBehaviour {
 		StartCoroutine(recordLocationHelper ());
 	}
 
-	public void executeDeathCycle(Vector3[] locs){
+	public void executeDeathCycle(Vector3[] locs, Quaternion[] rots){
 		Debug.Log ("Death Cycle:" + locs[0]);
 		gameObject.transform.position = locs[0];
+		gameObject.transform.rotation = rots [0];
 
 		StartCoroutine(moveFunction(locs));
+		StartCoroutine(rotateFunction(rots));
+	}
+
+	public IEnumerator rotateFunction(Quaternion[] rotations){
+		int i = 0;
+
+		Quaternion rotA = gameObject.transform.rotation;
+		Quaternion rotB = rotations [i];
+
+		float timeSinceStarted = 0f;
+		while (i < rotations.Length) {
+			Debug.Log ("Lerping: " + i);
+			timeSinceStarted += Time.deltaTime;
+			gameObject.transform.rotation = Quaternion.Lerp (rotA, rotB, timeSinceStarted);
+			//If arrived, stop coroutine
+			if (gameObject.transform.rotation == rotB) {
+				rotA = gameObject.transform.rotation;
+				rotB = rotations [i++];
+				timeSinceStarted = 0f;
+				//yield break;
+			}
+			yield return null;
+			Debug.Log ("Return Null");
+		}
 	}
 
 	public IEnumerator moveFunction(Vector3[] positions){
@@ -68,13 +95,14 @@ public class CloneBehavior : MonoBehaviour {
 			Debug.Log ("Entered While Loop");
 			Debug.Log (dynamicArraySize);
 			locationArray[i] = (player.transform.position);
+			rotationArray [i] = (player.transform.rotation);
 			i++;
 			yield return new WaitForSeconds (recordDelay);
 		}
 
 		Debug.Log ("Exited While Loop");
 		GameObject clone = (GameObject)Instantiate (clonePlayer, transform.position, transform.rotation);
-		clone.GetComponent<CloneBehavior> ().executeDeathCycle(locationArray);
+		clone.GetComponent<CloneBehavior> ().executeDeathCycle(locationArray, rotationArray);
 	}
 
 }
